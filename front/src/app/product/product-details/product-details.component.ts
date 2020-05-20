0cedc27138213ae4e0234bb7f05abae2e3baa364
chinamiee <38914459+chinamiee@users.noreply.github.com>
2020-05-20 19:15:30 +0200

    instalation de bootstrap md// update produit detail// produit ts// produit service

--- front/src/app/product/product-details/product-details.component.ts
+++ front/src/app/product/product-details/product-details.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute, Router } from '@angular/router';
+import { ProductService } from 'src/services/product/product.service';
+import { Product } from 'src/Models/product' ;
 @Component({
   selector: 'app-product-details',
   templateUrl: './product-details.component.html',
@@ -10,9 +12,31 @@ export class ProductDetailsComponent implements OnInit {
   size: string;
   listecolors = ['White', 'Black', 'Red', 'Orange', 'Yellow', 'Green', 'Blue', 'Indigo', 'Purple'];
   color: string;
-  constructor() { }
+  product: Product;
+  productId: number;
+  sizeId: number;
+  colorId: number;
 
-  ngOnInit(): void {
+
+
+  constructor(private productService: ProductService, private route: ActivatedRoute) {
+      this.product = new Product();
+
+    }
+
+    ngOnInit() {
+      this.route.params.subscribe(params => {
+        this.productId = params [' id '];
+      } )
+      this.getProductDetailsById();
+    }
+    getProductDetailsById(){
+      this.productService.getProductDetailsById(this.productId)
+      .subscribe(p => {
+        this.product = p as Product;
+        this.colorId = p.Color[0].AttributeValueId;
+        this.sizeId = p.Size[0].AttributeValueId;
+        console.log(this.product);
+      })
+    }
   }
-
-}
--- front/src/services/product/product.service.ts
+++ front/src/services/product/product.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Product } from 'src/Models/product';
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
   url = localStorage.getItem('ServerUrl');
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+// productdetail
+  getProductDetailsById(productId: number): Observable<Product> {
+    return this.http.get<Product>(`${this.url}product/getProductDetails?productId=${productId}`);
+  }
+
+
 }
